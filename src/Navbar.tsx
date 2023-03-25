fa6a9b11653416a526d56b54296ca062e777c3ba
Long Phan <111166256+longphanquangminh@users.noreply.github.com>
2023-03-25 14:11:31 +0700

    done front-end navbar all animation & transition

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react"
 
 function Navbar() {
   const [changeNavbarColor, setChangeNavbarColor] = useState(false)
-
+  const [hoverNavbar, setHoverNavbar] = useState(false)
+  console.log(hoverNavbar)
   useEffect(() => {
     
     const handleScroll = () => {
@@ -14,26 +15,29 @@ function Navbar() {
     }
 
     window.addEventListener('scroll', handleScroll)
+    // window.addEventListener('mouseover', handleHover)
 
-    return () =>
-    window.removeEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+      // window.removeEventListener('mouseover', handleHover)
+    }
 
   }, [])
-  const navBarStyles = changeNavbarColor ? 'bg-white text-neutral-600' : 'text-white'
-  const classes = `px-12 py-6 flex justify-between  text-sm font-light fixed top-0 left-0 right-0 ${navBarStyles} hover:bg-white hover:text-neutral-600`
+
   return (
     <>
       <div className="">
         <video className= "relative" autoPlay muted loop src="https://cdn.shopify.com/videos/c/o/v/a1e944e243cd4e06b50e5eebd96d0d2f.mp4"></video>
 
-        <nav className={classes}>
+        <nav className={`px-12 py-6 flex duration-300 justify-between  text-sm font-light fixed top-0 left-0 right-0 ${changeNavbarColor ? 'bg-white text-neutral-600' : 'text-white'} hover:bg-white hover:text-neutral-600`}  onMouseOver={() => setHoverNavbar(true)} 
+        onMouseLeave={() => setHoverNavbar(false)}>
             <ul className="uppercase flex">
               <li><a href="category.html">shop</a></li>
               <li className="pl-8"><a>kits</a></li>
               <li className="pl-8"><a href="tech.html">tech</a></li>
               <li className="pl-8"><a>explore</a></li>
             </ul>
-            <img src="https://cdn.shopify.com/s/files/1/0297/6293/files/Wings_ASRV_NEW_d5bba963-30a6-4d73-ba2e-68d1a8ea69c4_120x.png?v=1664577873" className="justify-self-center text-3xl" style={{filter: changeNavbarColor ? '' : 'brightness(0) invert(1)'}} />
+            <img src="https://cdn.shopify.com/s/files/1/0297/6293/files/Wings_ASRV_NEW_d5bba963-30a6-4d73-ba2e-68d1a8ea69c4_120x.png?v=1664577873" className={`justify-self-center text-3xl duration-300 ${changeNavbarColor || hoverNavbar ? '' : 'brightness-200 invert-0'}`} />
             <ul className="capitalize flex font-light ">
               <li><a href="account.html">account</a></li>
               <li className="pl-8"><a>search</a></li>
