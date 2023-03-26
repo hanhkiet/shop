6b6ca787c27c42493aef2f34d1c015456f8edc0f
Long Phan <111166256+longphanquangminh@users.noreply.github.com>
2023-03-26 22:07:05 +0700

    responsive video with text & edit w, h top button

--- src/Cover.tsx
+++ src/Cover.tsx
@@ -3,12 +3,14 @@ function Cover() {
   return (
     <>
      <div className="relative">
-        <video className= "" autoPlay loop muted src="https://cdn.shopify.com/videos/c/o/v/a1e944e243cd4e06b50e5eebd96d0d2f.mp4"></video>
-        <div className="absolute  uppercase  bottom-10 mx-20">
-          <h1 className="pb-2 text-3xl text-white font-bold">Spring '23 Collection</h1>
-          <h3 className="text-white pb-3 font-light tracking-widest">The Art of Adaptation</h3>
-          <button className="uppercase bg-white text-gray-500 tracking-widest text-sm px-9 py-3 hover:bg-gray-300 ">Shop Now</button>
-          <button className="uppercase bg-neutral-600 text-white tracking-widest text-sm px-9 py-3 hover:bg-black mx-3">Editorial</button>
+        <video className= "right-0 bottom-0 min-w-full min-h-full" autoPlay loop muted src="https://cdn.shopify.com/videos/c/o/v/a1e944e243cd4e06b50e5eebd96d0d2f.mp4"></video>
+        <div className="absolute uppercase bottom-3 md:bottom-40 lg:bottom-48 sm:mx-0 md:mx-20 lg:mx-20 text-center md:text-left lg:text-left w-full md:w-max lg:w-max">
+          <h1 className="pb-2 md:text-3xl lg:text-3xl text-white font-bold text-xs">Spring '23 Collection</h1>
+          <h3 className="text-white md:text-1xl lg:text-1xl pb-3 font-light tracking-widest text-xs">The Art of Adaptation</h3>
+          <div className="flex justify-between md:justify-start lg:justify-start mx-5 md:mx-0 lg:mx-0">
+            <button className="uppercase bg-white text-gray-500 tracking-widest text-xs px-5 py-3 hover:bg-gray-300"><span>Shop Now</span></button>
+            <button className="uppercase bg-neutral-600 text-white tracking-widest text-xs px-5 py-3 hover:bg-black mx-0 md:mx-3 lg:mx-3"><span>Editorial</span></button>
+          </div>
         </div>
       </div>
     </>
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -36,7 +36,7 @@ function Navbar() {
   return (
     <>
     <Cover />
-        <nav className={`px-12 py-6 flex duration-300 justify-between text-sm font-light fixed top-0 left-0 right-0 ${changeNavbarColor ? 'bg-white text-neutral-600' : 'text-white'} hover:bg-white hover:text-neutral-600`}  onMouseOver={() => setHoverNavbar(true)} 
+        <nav className={`px-6 py-6 flex duration-300 justify-between text-sm font-light fixed top-0 left-0 right-0 ${changeNavbarColor ? 'bg-white text-neutral-600' : 'text-white'} hover:bg-white hover:text-neutral-600`}  onMouseOver={() => setHoverNavbar(true)} 
         onMouseLeave={() => setHoverNavbar(false)}>
             <ul className="uppercase hidden md:flex lg:flex">
               <li><a href="category.html">shop</a></li>
@@ -45,24 +45,24 @@ function Navbar() {
               <li className="pl-8"><a>explore</a></li>
             </ul>
             <ul className="uppercase flex md:hidden lg:hidden">
-              <li><img src="https://cdn-icons-png.flaticon.com/512/6015/6015685.png" className={`h-5 mx-auto duration-300 ${changeNavbarColor || hoverNavbar ? '' : 'grayscale invert'}`} /></li>
-              <li className="pl-8"><img src="https://media.discordapp.net/attachments/1026660684739653674/1089365167730602095/cart.png" className={`h-5 mx-auto duration-300 ${changeNavbarColor || hoverNavbar ? '' : 'grayscale invert'}`} /></li>
+              <li><img src="https://cdn-icons-png.flaticon.com/512/6015/6015685.png" className={`h-4 mx-auto duration-300 ${changeNavbarColor || hoverNavbar ? '' : 'grayscale invert'}`} /></li>
+              <li className="pl-8"><img src="https://media.discordapp.net/attachments/1026660684739653674/1089365167730602095/cart.png" className={`h-4 mx-auto duration-300 ${changeNavbarColor || hoverNavbar ? '' : 'grayscale invert'}`} /></li>
             </ul>
-            <img src="https://cdn.shopify.com/s/files/1/0297/6293/files/Wings_ASRV_NEW_d5bba963-30a6-4d73-ba2e-68d1a8ea69c4_120x.png?v=1664577873" className={`mx-auto h-6 duration-300 ${changeNavbarColor || hoverNavbar ? '' : 'brightness-200'}`} />
+            <img src="https://cdn.shopify.com/s/files/1/0297/6293/files/Wings_ASRV_NEW_d5bba963-30a6-4d73-ba2e-68d1a8ea69c4_120x@2x.png?v=1664577873" className={`mx-auto h-5 duration-300 ${changeNavbarColor || hoverNavbar ? '' : 'brightness-200'}`} />
             <ul className="capitalize font-light hidden md:flex lg:flex">
               <li><a href="account.html">account</a></li>
               <li className="pl-8"><a>search</a></li>
               <li className="pl-8"><a>cart({0})</a></li>
             </ul>
             <ul className="uppercase flex md:hidden lg:hidden">
-              <li><img src="https://cdn-icons-png.flaticon.com/512/149/149852.png" className={`h-5 mx-auto duration-300 ${changeNavbarColor || hoverNavbar ? '' : 'grayscale invert'}`} /></li>
-              {/* <li className="pl-8"><img src="https://cdn-icons-png.flaticon.com/512/419/419910.png" className={`h-5 mx-auto duration-300 ${changeNavbarColor || hoverNavbar ? '' : 'grayscale invert'}`} /></li> */}
-              <li className="pl-8"><img src="https://media.discordapp.net/attachments/1026660684739653674/1089228771149762690/cart_has_product.png" className={`h-5 mx-auto duration-300 ${changeNavbarColor || hoverNavbar ? '' : 'grayscale invert'}`} /></li>
+              <li><img src="https://cdn-icons-png.flaticon.com/512/149/149852.png" className={`h-4 mx-auto duration-300 ${changeNavbarColor || hoverNavbar ? '' : 'grayscale invert'}`} /></li>
+              {/* <li className="pl-8"><img src="https://cdn-icons-png.flaticon.com/512/419/419910.png" className={`h-4 mx-auto duration-300 ${changeNavbarColor || hoverNavbar ? '' : 'grayscale invert'}`} /></li> */}
+              <li className="pl-8"><img src="https://media.discordapp.net/attachments/1026660684739653674/1089228771149762690/cart_has_product.png" className={`h-4 mx-auto duration-300 ${changeNavbarColor || hoverNavbar ? '' : 'grayscale invert'}`} /></li>
             </ul>
           </nav>
 
   {changeNavbarColor && (
-    <button className="fixed bottom-5 right-5 h-16 w-16 rounded-full bg-blue-500 hover:bg-red-500 text-white" onClick={scrollToTop}><img src="https://cdn-icons-png.flaticon.com/512/608/608336.png" className="h-5 grayscale invert flex mx-auto" /></button>
+    <button className="fixed bottom-5 right-5 h-12 w-12 rounded-full bg-blue-500 hover:bg-red-500 text-white" onClick={scrollToTop}><img src="https://cdn-icons-png.flaticon.com/512/608/608336.png" className="h-5 grayscale invert flex mx-auto" /></button>
   )}
     </>
   )
