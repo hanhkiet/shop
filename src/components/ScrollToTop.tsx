a9411b94f27e87e41ed1eaf56d6fa59cc61cf52d
Long Phan <111166256+longphanquangminh@users.noreply.github.com>
2023-04-18 22:55:02 +0700

    [Feature] SHOP-35

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react'
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+export default function ScrollToTop() {
+    const [scrollPercentageChange, setScrollPercentageChange] = useState(0);
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth', // for smoothly scrolling
+        });
+    };
+    useEffect(() => {
+        const h = document.documentElement,
+            b = document.body,
+            st = 'scrollTop',
+            sh = 'scrollHeight';
+
+        const handleScroll = () => {
+            setScrollPercentageChange(
+                ((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight)) * 100,
+            );
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        // window.addEventListener('mouseover', handleHover)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+            // window.removeEventListener('mouseover', handleHover)
+        };
+    }, []);
+
+    return (
+        <button
+            className="fixed bottom-5 right-5 z-30 h-12 w-12 rounded-full bg-gray-500 text-white hover:bg-gray-400"
+            onClick={scrollToTop}
+            title="Click to return to top page"
+        >
+            <CircularProgressbarWithChildren value={scrollPercentageChange}>
+                <img
+                    src="https://cdn-icons-png.flaticon.com/512/608/608336.png"
+                    className="mx-auto flex h-5 grayscale invert"
+                    alt=""
+                />
+            </CircularProgressbarWithChildren>
+        </button>
+    )
+}
+
